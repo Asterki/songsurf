c695b7fdead4f73584ef2f735531ff5c73d281d1
Fernando Rivera <asterki.dev@proton.me>
2023-11-23 11:23:16 -0600

    Feat: Head meta tags

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,6 +1,22 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+import { appWithTranslation } from "next-i18next";
+
+import Head from "next/head";
+
+const App = ({ Component, pageProps: { session, ...pageProps } }: any) => {
+    return (
+        <>
+            <Head>
+                <meta
+                    name="viewport"
+                    content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
+                />
+            </Head>
+
+            <Component {...pageProps} />
+        </>
+    );
+};
+
+export default appWithTranslation(App);
--- client/src/pages/_document.tsx
+++ client/src/pages/_document.tsx
@@ -1,13 +1,55 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+    return (
+        <Html lang="en">
+            <Head>
+                <meta name="application-name" content="songsurf" />
+                <meta
+                    name="description"
+                    content="SongSurf is a music recommendation app designed to ride the waves of your musical preferences. Dive into a sea of personalized playlists and discover a harmonious journey through your favorite genres and artists."
+                />
+                <meta name="format-detection" content="telephone=no" />
+                <meta name="msapplication-TileColor" content="#27363B" />
+                <meta name="msapplication-tap-highlight" content="no" />
+                <meta name="theme-color" content="#000000" />
+
+                <link
+                    rel="shortcut icon"
+                    href="/favicon.ico"
+                    type="image/x-icon"
+                />
+                <link rel="shortcut icon" href="/favicon.ico" />
+
+                <meta name="twitter:card" content="summary" />
+                <meta name="twitter:url" content="https://songsurf.com" />
+                <meta name="twitter:title" content="songsurf" />
+                <meta
+                    name="twitter:description"
+                    content="SongSurf is a music recommendation app designed to ride the waves of your musical preferences. Dive into a sea of personalized playlists and discover a harmonious journey through your favorite genres and artists."
+                />
+                <meta
+                    name="twitter:image"
+                    content="https://songsurf.com/logos/android/android-launchericon-192-192.png"
+                />
+                <meta name="twitter:creator" content="@AsterkiDev" />
+                <meta property="og:type" content="website" />
+                <meta property="og:title" content="songsurf" />
+                <meta
+                    property="og:description"
+                    content="SongSurf is a music recommendation app designed to ride the waves of your musical preferences. Dive into a sea of personalized playlists and discover a harmonious journey through your favorite genres and artists."
+                />
+                <meta property="og:site_name" content="songsurf" />
+                <meta property="og:url" content="https://songsurf.com" />
+                <meta
+                    property="og:image"
+                    content="https://songsurf.com/logos/ios/512.png"
+                />
+            </Head>
+            <body>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    );
 }
