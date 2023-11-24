0bc0a82f16eebff0b4dccac36ca73b5d7d6ea430
Fernando Rivera <asterki.dev@proton.me>
2023-11-24 10:28:21 -0600

    Refactor: Code Cleaning

--- algorithm/src/index.py
+++ algorithm/src/index.py
@@ -2,34 +2,39 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS, cross_origin
 import pandas as pd
 
+# Create the API endpoint
 app = Flask(__name__)
 cors = CORS(app)
 
+# Load the song database
+try:
+    songs = pd.read_csv(
+        "https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-01-21/spotify_songs.csv"
+    )
+except Exception as e:
+    print(f"Error loading data: {e}")
+    songs = pd.DataFrame()
 
-# Range values for each criterion
-valence_range = 0.05
-danceability_range = 0.1
-energy_range = 0.1
 
+class SongFilter:
+    def __init__(self, valence_range, danceability_range, energy_range, songs):
+        self.songs = songs
+        self.valence_range = valence_range
+        self.danceability_range = danceability_range
+        self.energy_range = energy_range
 
-class Filter:
-    def __init__(self):
-        self.songs = pd.read_csv(
-            "https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-01-21/spotify_songs.csv"
-        )
-
-    def filter(self, valence, danceability, energy, instrumental, genre, subgenre):
+     def filter(self, valence, danceability, energy, instrumental, genre, subgenre):
         # Filter the DataFrame based on multiple criteria
         filtered_songs = self.songs[
             # Valence
-            (self.songs["valence"] >= valence - valence_range)
-            & (self.songs["valence"] <= valence + valence_range)
+            (self.songs["valence"] >= valence - self.valence_range)
+            & (self.songs["valence"] <= valence + self.valence_range)
             # Danceability
-            & (self.songs["danceability"] >= danceability - danceability_range)
-            & (self.songs["danceability"] <= danceability + danceability_range)
+            & (self.songs["danceability"] >= danceability - self.danceability_range)
+            & (self.songs["danceability"] <= danceability + self.danceability_range)
             # Energy
-            & (self.songs["energy"] >= energy - energy_range)
-            & (self.songs["energy"] <= energy + energy_range)
+            & (self.songs["energy"] >= energy - self.energy_range)
+            & (self.songs["energy"] <= energy + self.energy_range)
             # Instrumentalness
             & (
                 self.songs["instrumentalness"] > 0
@@ -44,23 +49,33 @@ class Filter:
         return filtered_songs
 
 
-filter = Filter()
+song_filter = SongFilter(valence_range=0.1, danceability_range=0.1, energy_range=0.1, songs=songs)
 
 
 @app.route("/api/search", methods=["POST"], strict_slashes=False, endpoint="search")
 @cross_origin()
 def search():
-    data = request.get_json()
+    # Check the request values
+    data = request.get_json(force=True)
+    if not data:
+        return jsonify({"error": "Empty request"}), 400
 
-    filtered_songs = filter.filter(
-        data["valence"] / 100,
-        data["danceability"] / 100,
-        data["energy"] / 100,
-        data["instrumental"],
-        data["genre"],
-        data["subgenre"],
-    )
+    required_keys = ["valence", "danceability", "energy", "instrumental", "genre", "subgenre"]
+    if not all(key in data for key in required_keys):
+        return jsonify({"error": "Missing required key(s) in request"}), 400
 
+
+    filter_params = {
+        "valence": data["valence"] / 100,
+        "danceability": data["danceability"] / 100,
+        "energy": data["energy"] / 100,
+        "instrumental": data["instrumental"],
+        "genre": data["genre"],
+        "subgenre": data["subgenre"],
+    }
+
+    # Get the songs and return them
+    filtered_songs = song_filter.filter(**filter_params)
     return jsonify(filtered_songs.to_dict(orient="records"))
 
 
